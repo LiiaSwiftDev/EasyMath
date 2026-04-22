4d8c1f2e6c72af1da377f970729345a1877a0bcc
Liya <liiakoshelenko1@gmail.com>
2026-04-22 08:33:50 -0700

    Initial Commit

---
+++ EasyMath/EasyMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyMathApp.swift
+//  EasyMath
+//
+//  Created by Лия Кошеленко on 2026-04-22.
+//
+
+import SwiftUI
+
+@main
+struct EasyMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
