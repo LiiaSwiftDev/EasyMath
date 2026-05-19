279f05d1f50c996dd83800dcbce7dc86e8793ebf
Liya <liiakoshelenko1@gmail.com>
2026-05-19 11:24:54 -0700

    - Use a navigation path in MainView to handle screen navigation - Create a "Start Over" button to restart the quiz - Implement a "Back to Home" button to return to the main screen

---
+++ EasyMath/Result/StartOverButton.swift
@@ -0,0 +1,18 @@
+//
+//  StartOverButton.swift
+//  EasyMath
+//
+//  Created by Лия Кошеленко on 2026-05-19.
+//
+
+import SwiftUI
+
+struct StartOverButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartOverButton()
+}
